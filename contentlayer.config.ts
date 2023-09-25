2bead009c5c5c0d91a7eea80ffdeeadfed860f2c
Mani Velrajan <mani.velrajan.ctr@ihstowers.com>
2023-09-25 23:33:19 +0530

    ContentLayer installation setup and content files uploaded

---
+++ contentlayer.config.ts
@@ -0,0 +1,46 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+export const Blog = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: `**/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+    publishedAt: {
+      type: "date",
+      required: true,
+    },
+    updatedAt: {
+      type: "date",
+      required: true,
+    },
+    image: { type: "image" },
+    isPublished: {
+      type: "boolean",
+      default: true,
+    },
+    author: {
+      type: "string",
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+    },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (doc) => `/blogs/${doc._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({ contentDirPath: "content", documentTypes: [Blog] });
